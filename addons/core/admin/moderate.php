ecb56f77687073ae1637517e2e09d9e7250ba15e
Greg Winiarski <grzewin@gmail.com>
2021-07-22 13:48:43 +0200

    Version 1.5.5

---
+++ addons/core/admin/moderate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Displays Core Options Page
+ * 
+ * This file is a template for wp-admin / Classifieds / Options / Core panel. 
+ * 
+ * It is being loaded by adext_core_page_options function.
+ * 
+ * @see adext_core_page_options()
+ * @since 0.1
+ */
+?>
+<div class="wrap">
+    <h2 class="nav-tab-wrapper">
+        <a href="<?php esc_attr_e( remove_query_arg( array( 'adaction' ) ) ) ?>" class="nav-tab"><?php _e("Core Options", "wpadverts") ?></a>
+        <a href="<?php esc_attr_e( add_query_arg( array('adaction' => 'gallery') ) ) ?>" class="nav-tab "><?php _e("Gallery", "wpadverts") ?></a>
+        <a href="<?php esc_attr_e( add_query_arg( array('adaction' => 'types') ) ) ?>" class="nav-tab "><?php _e("Types", "wpadverts") ?></a>
+        <a href="<?php esc_attr_e( add_query_arg( array('adaction' => 'moderate') ) ) ?>" class="nav-tab nav-tab-active"><?php _e("Spam", "wpadverts") ?></a>
+    </h2>
+
+    <?php adverts_admin_flash() ?>
+
+    <form action="" method="post" class="adverts-form">
+        <table class="form-table">
+            <tbody>
+            <?php echo adverts_form_layout_config($form) ?>
+            </tbody>
+        </table>
+
+        <p class="submit">
+            <input type="submit" value="<?php echo esc_attr($button_text) ?>" class="button-primary" name="Submit" />
+        </p>
+
+    </form>
+
+</div>
---
+++ includes/class-honeypot.php
@@ -0,0 +1,73 @@
+<?php
+
+class WPAdverts_Honeypot {
+
+    public function __construct() {
+        add_action( "adverts_form_load", array( $this, "form_load" ), 5000 );
+        add_action( "wp_head", array( $this, "head" ) ); 
+        
+        do_action( "wpadverts_honeypot_init", $this );
+    }
+
+    public function get_supported_forms() {
+        return apply_filters( "wpadverts_honeypot_forms", array( "advert", "contact" ) );
+    }
+
+    public function register_field_and_validator() {
+
+        adverts_form_add_validator("wpadverts_honeypot_validator", array(
+            "callback" => array( $this, "validator" ),
+            "label" => "",
+            "params" => array(),
+            "default_error" => __( "Are you a bot trying to submit a classified Ad?", "wpadverts" ),
+            "validate_empty" => false
+        ));
+    }
+
+    public function form_load( $form ) {
+
+        if( ! in_array( $form["name"], $this->get_supported_forms() ) ) {
+            return $form;
+        }
+
+        $form["field"][] = apply_filters( "wpadverts_honeypot_input", array(
+            "name" => adverts_config( "moderate.honeypot_name"),
+            "type" => "adverts_field_text",
+            "label" => adverts_config( "moderate.honeypot_title" ),
+            "value" => "",
+            "order" => 9999,
+            "validator" => array(
+                array( "name" => "wpadverts_honeypot_validator" )
+            ),
+            "attr" => array(
+                "autocomplete" => "off"
+            )
+        ) );
+
+        return $form;
+    }
+    
+    public function validator( $data ) {
+        if( strlen( trim( $data ) ) > 0 ) {
+            add_filter( "adverts_flash_data", array( $this, "flash_data" ) );
+            return "invalid";
+        } else {
+            return true;
+        }
+    }
+
+    public function flash_data( $data ) {
+        $data["error"] = array(
+            array(
+                "message" => __( "Are you a bot trying to submit a classified Ad?", "wpadverts" ),
+                "icon" => "adverts-icon-user-secret"
+            )
+        );
+        return $data;
+    }
+
+    public function head() {
+        echo sprintf( '<style type="text/css">.adverts-field-name-%s { display: none !important }</style>', adverts_config( "moderate.honeypot_name" ) );
+    }
+}
+

f5cd8e088a71a029528b1d42f26b838308d950c7
DAnn2012 <dannunziata@hotmail.it>
2025-09-22 10:33:08 +0200

    Update payments.php

--- addons/payments/payments.php
+++ addons/payments/payments.php
@@ -439,7 +439,7 @@ function adverts_payments_field_payment($field) {
                         <?php if( $visible > 0 ): ?>
                         <?php printf( _n("Visible 1 day", "Visible %d days", $visible, "wpadverts"), $visible) ?>
                         <?php else: ?>
-                        <?php echo esc_html_e( "Never Expires", "wpadverts" ) ?>
+                        <?php esc_html_e( "Never Expires", "wpadverts" ) ?>
                         <?php endif; ?>
                     </span>
 
@@ -520,7 +520,7 @@ function adverts_payments_field_payment_block( $field ) {
                         <?php if( $visible > 0 ): ?>
                         <?php printf( _n("Visible 1 day", "Visible %d days", $visible, "wpadverts"), $visible) ?>
                         <?php else: ?>
-                        <?php echo esc_html_e( "Never Expires", "wpadverts" ) ?>
+                        <?php esc_html_e( "Never Expires", "wpadverts" ) ?>
                         <?php endif; ?>
                     </span>
                     <?php do_action("adverts_payments_features", $post->ID ) ?>
@@ -1634,4 +1634,5 @@ function adext_payments_format_order_id( $post ) {
     }
     
     return apply_filters("adext_payments_format_order_id", "#".str_pad($post_id, 6, "0", STR_PAD_LEFT), $post_id );
+
 }
